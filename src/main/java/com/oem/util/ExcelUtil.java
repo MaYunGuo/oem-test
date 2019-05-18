213fcfbf0cf7394f2eec2b99eefb3f3d96a9784e
xiaobin.Guo <374295981@qq.com>
2019-05-18 15:02:52 +0800

    测试

--- src/main/java/com/oem/util/ExcelUtil.java
+++ src/main/java/com/oem/util/ExcelUtil.java
@@ -1,21 +1,15 @@
 package com.oem.util;
 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.math.BigDecimal;
 import java.text.DateFormat;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class ExcelUtil {
@@ -52,10 +46,10 @@ public class ExcelUtil {
         if (cell != null) {
             switch (cell.getCellType()) {
                 case Cell.CELL_TYPE_NUMERIC:
-                    if (DateUtil.isCellDateFormatted(cell)) {
+                    if (org.apache.poi.ss.usermodel.DateUtil.isCellDateFormatted(cell)) {
                         cellValue = formater.format(cell.getDateCellValue());
                     } else {
-                        cellValue = String.valueOf(cell.getNumericCellValue());
+                        cellValue=new BigDecimal(cell.getNumericCellValue()+"").toString();
                     }
                     break;
                 case Cell.CELL_TYPE_STRING:
@@ -96,6 +90,54 @@ public class ExcelUtil {
               dataList.add(data);
         }
         return dataList;
+    }
+
+
+    public static void exportExcel(String filePath, String fileNamePrefix, List<String> header, List<String[]> dataValues) throws Exception {
+
+        Workbook wb = new XSSFWorkbook();
+        // 第二步，在workbook中添加一个sheet,对应Excel文件中的sheet
+        Sheet sheet = wb.createSheet();
+        // 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
+        Row row = sheet.createRow(0);
+        // 第四步，创建单元格，并设置值表头 设置表头居中
+        CellStyle style = wb.createCellStyle();
+        style.setAlignment(HorizontalAlignment.CENTER);
+        Cell cell = null;
+
+        //创建标题
+        for(int i=0;i<header.size();i++){
+            cell = row.createCell(i);
+            cell.setCellValue(header.get(i));
+            cell.setCellStyle(style);
+        }
+        //创建内容
+        for(int i=0;i<dataValues.size();i++){
+            row = sheet.createRow(i + 1);
+            for(int j=0;j<dataValues.get(i).length;j++){
+                //将内容按顺序赋给对应的列对象
+                row.createCell(j).setCellValue(dataValues.get(i)[j]);
+            }
+        }
+        //创建上传文件目录
+        File folder = new File(filePath);
+        //如果文件夹不存在创建对应的文件夹
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        //设置文件名
+        String fileName = fileNamePrefix  + DateUtil.getRvTime() + ".xlsx";
+        String savePath = filePath + File.separator + fileName;
+        File outFile = new File(savePath);
+        if(!outFile.exists()) {
+            outFile.createNewFile();
+            outFile = new File(savePath);
+        }
+        OutputStream fileOut = new FileOutputStream(outFile);
+        wb.write(fileOut);
+        fileOut.close();
+        wb.close();
     }
 
 
